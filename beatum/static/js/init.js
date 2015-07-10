4911be42a43f3cbc900ef5a3120b4cf109b9c61f
beatum <direct@beatum-group.ru>
2015-07-10 13:50:34 +0300

    Fix style and add blog templates

--- beatum/static/js/init.js
+++ beatum/static/js/init.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
             animationSpeed: 200,         // Animation speed (ms)
             scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
             scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
-            scrollText: 'Вверх', // Text for element, can contain HTML
+            scrollText: '<i class=\"fa fa-angle-up\"></i>', // Text for element, can contain HTML
             scrollTitle: false,          // Set a custom <a> title if required.
             scrollImg: false,            // Set true to use image
             activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
